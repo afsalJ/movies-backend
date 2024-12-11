1aab1b5e8010f98934bc92825234b73eb51ba99d
Poovarasang123 <poovarasangl123@gmail.com>
2024-12-11 06:34:59 +0530

    test commit

--- src/main/java/com/afsal/MovieApi/config/WebConfig.java
+++ src/main/java/com/afsal/MovieApi/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    //test
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Apply CORS for specific endpoints
